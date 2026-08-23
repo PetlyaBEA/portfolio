08c0d501660838b84692edd016c9a850e8ebc351
Cris <baukaepen1@gmail.com>
2026-08-23 17:48:25 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Relative asset paths work both at username.github.io and username.github.io/repository/.
-  base: './',
+  base: '/portfolio/',
 })
